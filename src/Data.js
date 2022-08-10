d7322887e9a29022c5febeb95fe6a38bbc757dc5
Finalcreature <finalnadav@gmail.com>
2022-08-10 15:22:04 +0300

    Remove teams

--- src/Data.js
+++ src/Data.js
@@ -14,18 +14,18 @@ const voices = [{}];
 let currentRole = null;
 let isAdded = true;
 
-function evaluateRole(role) {
+function checkVoice(role) {
   if (role) {
     if (role !== currentRole) {
       {
         if (
-          role.toLowerCase().includes("voice") ||
-          role.toLowerCase().includes("actor")
+          role.textContent.toLowerCase().includes("voice") ||
+          role.textContent.toLowerCase().includes("actor")
         ) {
           currentRole = role;
-          isAdded = window.confirm("Include " + role);
+          isAdded = window.confirm("Include " + role.textContent);
           if (!isAdded) {
-            console.log("didn't add " + role);
+            console.log("didn't add " + role.textContent);
           }
         } else isAdded = true;
       }
@@ -44,27 +44,51 @@ function toSingular(role) {
   return role;
 }
 
+function checkTeam(role) {
+  if (role.textContent.includes("Team")) {
+    role.textContent = role.textContent.replace("Team", "");
+  }
+  return role;
+}
+
+function evaluateRole(role) {
+  if (role) {
+    role = checkTeam(role);
+    role = toSingular(role);
+    return role;
+  }
+}
+
 function getRelevantData() {
   const item = document.querySelectorAll("a:not(.CSV-link)");
 
   try {
     console.log(item[0].textContent);
     for (let index = 0; index < item.length; index++) {
-      let person = new Personal(
-        item[index].textContent,
-        toSingular(
-          item[index].parentElement.parentElement.childNodes[0]
-        ).textContent
-      );
-      if (
-        evaluateRole(
-          item[index].parentElement.parentElement.childNodes[0].textContent
-        )
-      ) {
+      if (checkVoice(item[index].parentElement.parentElement.childNodes[0])) {
+        let person = new Personal(
+          item[index].textContent,
+          evaluateRole(
+            item[index].parentElement.parentElement.childNodes[0]
+          ).textContent
+        );
         personnels.push(person);
       } else {
+        let person = new Personal(
+          item[index].textContent,
+          item[index].parentElement.parentElement.childNodes[0].textContent
+        );
         voices.push(person);
       }
+      // if (
+      //   checkVoice(
+      //     item[index].parentElement.parentElement.childNodes[0].textContent
+      //   )
+      // ) {
+      //   personnels.push(person);
+      // } else {
+      //   voices.push(person);
+      // }
     }
     console.log(personnels);
     console.log(voices);
